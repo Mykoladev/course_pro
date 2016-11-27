7b2d2db676c5b49df06b2c40fe686c6cad036df1
Mykoladev <Myukolanet@gmail.com>
2016-11-27 20:57:05 +0200

    fix dz_5

--- DZ_5/jquery.blurPlug.js
+++ DZ_5/jquery.blurPlug.js
@@ -14,132 +14,79 @@
 
 			var it = this;
 			var section = $(this).parent();
-			
-
 			$(it).append('<div class="dots"></div>');	
 	        var images = [ '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
 			for( var i = 0; i < images.length; i++ ){
-			$('.dots').append('<div data-img="'+i+'" class="dot"></div>');
+					$('.dots').append('<div data-img="'+i+'" class="dot"></div>');
 			}
 
-
 			$(it).append('<div class="leftbutton"><</div>');
-			$('.leftbutton').css({
-		'position': 'absolute',
-		'width': '50px',
-		'height': '50px',
-		'background-color':'red',
+			$(it).append('<div class="rightbutton">></div>');
 		
 
-					})
-
-
-		$(it).append('<div class="rightbutton">></div>');
-		$('.rightbutton').css({
-		'position': 'absolute',
-		'width': '50px',
-		'height': '50px',
-		'background-color':'red',
-		'right':'0px'
-		
-		})
-
-
-
-
-$('.rightbutton').click(function(){
+			$('.rightbutton').click(function(){
+				
 				if(i>images.length-1){
-				 i=-1;
-	    	 }
-			$(it).css('background','url(img/'+images[i++]+')');
-			$('.dot').css('background-color', 'rgba(0, 136, 255, .7)');
-			$('.dot:eq('+i+')').css('background-color', 'red');
+						i=0;
+				}
+				console.log(i);
+				$('.dot').css('background-color', 'rgba(0, 136, 255, .7)');
+				$('.dot:eq('+i+')').css('background-color', 'red');
+				$(it).css('background','url(img/'+images[i++]+')');
+				var bgImg = $(it).css('background-image');
+				$('.filtered').css('background-image', bgImg);
+			})
+	
 			
-			console.log(i);
-							var bgImg = $(it).css('background-image');
-									
-									$('.filtered').css('background-image', bgImg);
-		})
+
+			$('.leftbutton').click(function(){
+				 i-=2;
+				if(i<0){
+				 i=images.length-1;
+	    		 }
+				console.log(i);
+				$('.dot').css('background-color', 'rgba(0, 136, 255, .7)');
+				$('.dot:eq('+i+')').css('background-color', 'red');
+				$(it).css('background','url(img/'+images[i--]+')');
+				var bgImg = $(it).css('background-image');
+				$('.filtered').css('background-image', bgImg);
+				i+=2;
+				
+			})
 
 
+			var i=0;
+			$('.dot').click(function(){
+				var j= $(this).attr('data-img');
+				$(it).css('background', 'url(img/'+images[j]+')');
+				i=j;
+				var bgImg = $(it).css('background-image');
+				$('.filtered').css('background-image', bgImg);
+			});
 
 
-		console.log(i);
-		$('.leftbutton').click(function(){
-			if(i<=0){
-				 i=images.length;
-	    	 }
-			$(it).css('background','url(img/'+images[i--]+')')
-			console.log(i);
-			$('.dot').css('background-color', 'rgba(0, 136, 255, .7)');
-			 $('.dot:eq('+i+')').css('background-color', 'red');
-			 
-								var bgImg = $(it).css('background-image');
-									
-									$('.filtered').css('background-image', bgImg);
-		})
+  			setInterval(function(){
+				$(it).animate({
+					// width: 0,
+					opacity: 0}, 500, function() {
+					if(i>images.length-1){
+						i=0;
+					}
+					console.log(i);
+				$('.dot').css('background-color', 'rgba(0, 136, 255, .7)');
+				$('.dot:eq('+i+')').css('background-color', 'red');
+				$(this).css('background','url(img/'+images[i++]+')').animate({
+					//  width: '100%',
+					opacity: 1},500, function(){
+						var bgImg = $(it).css('background-image');
+						$('.filtered').css('background-image', bgImg);	
+					});
 
+				
+				});
 
-
-
-var i=0;
-	$('.dot').click(function(){
-		var j= $(this).attr('data-img');
-		// $('.imgBlock').html("<img src="+images[j]+">")
-		$(it).css('background', 'url(img/'+images[j]+')');
-		i=j;
-					
-					
-					var bgImg = $(it).css('background-image');
-									
-									$('.filtered').css('background-image', bgImg);
-		});
-
-  	setInterval(function(){
-             $(it).animate({
-				// width: 0,
-			  	opacity: 0}, 1000, function() {
-			 	 if(i>images.length-1){
-					 i=0;
-		    	 }
-			  	console.log(i);
-			  $('.dot').css('background-color', 'rgba(0, 136, 255, .7)');
-			  $('.dot:eq('+i+')').css('background-color', 'red');		 
-			  $(this).css('background','url(img/'+images[i++]+')').animate({
-				//  width: '100%',
-				 opacity: 1},5000);
-
-
-				 					var bgImg = $(it).css('background-image');
-									$('.filtered').animate({
-										opacity: 0
-									},1000, function(){
-
-											
-									$('.filtered').css('background-image', bgImg).animate({
-										opacity: 1
-									},5000);
-
-
-									});
-
-
-
-				 
-									
-            });
-        }, 4000);
-
-
-
-
-
-
-
-
-
-
-
+       		}, 3000);
+			   
 			
 			var bgImg = $(it).css('background-image');
 			// $(this).find( 'img' ); *****
@@ -173,10 +120,6 @@ var i=0;
 				break;
 			}
 			
-
-
-
-
 		}
 
 		return this.each(start)
